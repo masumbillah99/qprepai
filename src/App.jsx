f36cbe813efa46718d2d4da038b93a83e9926598
masumbillah99 <masumbillahyusuf1621@gmail.com>
2025-05-17 23:03:26 +0600

    route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import toast, { Toaster } from 'react-hot-toast'
+import Home from './pages/Home/Home'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 const App = () => {
   return (
-    <div className='text-5xl'>App</div>
+    <div>
+      <Router>
+        <Routes>
+          {/* base route */}
+          <Route path='/' element={<Home />} />
+
+          {/* others route */}
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </Router>
+
+      <Toaster
+        toastOptions={{
+          className: '',
+          duration: 5000,
+          removeDelay: 1000,
+          style: {
+            fontSize: '13px'
+          }
+        }}
+      />
+    </div>
   )
 }
 
